3c9d0d31a5c37718a65183c846732f1b727cdd54
Meet Patel <meetaiims@yahoo.com>
2021-01-18 04:52:51 -0600

    add rudimentary / and /about routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 export default function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Route exact path="/"><Home></Home></Route>
+      <Route path="/about" render={() => <About/>} />
     </div>
-  )
+  );
 }
